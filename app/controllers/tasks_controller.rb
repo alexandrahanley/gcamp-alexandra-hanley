2c2d8e806a0ae11d08e5ed3862dce010807be25b
Alexandra Hanley <alexandrahanley@gmail.com>
2015-01-26 17:31:39 -0700

    add form, index and new pages

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -55,6 +55,7 @@ class TasksController < ApplicationController
   # DELETE /tasks/1.json
   def destroy
     @task.destroy
+
     respond_to do |format|
       format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
       format.json { head :no_content }
