f0dac001d9400de96624400096f94671e1d45760
Alexandra Hanley <alexandrahanley@gmail.com>
2015-01-27 18:20:27 -0700

    update view pages with links

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127155157) do
+ActiveRecord::Schema.define(version: 20150127232007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.date     "date"
+    t.boolean  "complete",    default: false
   end
 
 end
