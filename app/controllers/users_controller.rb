18658804c9b8ac65dd224ffe456c34d01532f6ec
Alexandra Hanley <alexandrahanley@gmail.com>
2015-02-04 08:50:04 -0700

    remove json from users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,17 +22,13 @@ class UsersController < ApplicationController
 
 def create
   @user = User.new(user_params)
-
-  respond_to do |format|
     if @user.save
-      format.html { redirect_to users_path, notice: 'User was successfully created.' }
-      format.json { render :show, status: :created, location: @user }
+      redirect_to users_path, notice: 'User was successfully created.'
     else
-      format.html { render :new }
-      format.json { render json: @user.errors, status: :unprocessable_entity }
+       render :new
     end
   end
-end
+
 
 
 
