953e89612b5583dbacd320a1666779e778c3211e
Alexandra Hanley <alexandrahanley@gmail.com>
2015-03-10 17:11:14 -0600

    user can add member to projects by selecting from all users and roles

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,7 +1,9 @@
 class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
-  
+
   enum role: {member: 0, owner: 1}
+
+  validates :user_id, presence: true, uniqueness: true
 
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
  has_many :memberships
  has_many :users, through: :memberships
 
-has_many :tasks
-  validates :name, presence: true
+ has_many :tasks
+ validates :name, presence: true
 
 end
