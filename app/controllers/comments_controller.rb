98451ff47484e548e4b73695f3a4c6d55ffb7830
Alexandra Hanley <alexandrahanley@gmail.com>
2015-04-07 22:19:35 -0600

    user can only add comment if description is present

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,7 +9,7 @@ class CommentsController < ApplicationController
       if @comment.save
       redirect_to project_task_path(@project, @task), notice: 'Comment was successfully created.'
       else
-        render :index
+        redirect_to project_task_path(@project, @task)
       end
     end
 
