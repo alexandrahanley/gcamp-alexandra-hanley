5fc91881ef3002ae5581d74c1ec3362a6d2cacbb
Alexandra Hanley <alexandrahanley@gmail.com>
2015-04-14 14:56:04 -0600

    update projects CRUD spec

--- spec/features/projects_page_spec.rb
+++ spec/features/projects_page_spec.rb
@@ -2,15 +2,17 @@ require 'rails_helper'
 
 describe 'User can CRUD projects' do
 
-  scenario 'User can create a project' do
-    @user = User.create(first_name: "V", last_name: "V", email: "v@v.com", password: "v", password_confirmation: "v", admin: :true)
-    visit '/'
-    click_on "Projects"
-    fill_in 'email', with: "v@v.com"
-    fill_in 'password', with: "v"
+  before :each do
+    User.create(first_name: "Alexandra", last_name: "Hanley",
+                email: "alexandra@gmail.com", password: "password", admin: true)
+    visit "/signin"
+    fill_in 'email', with: "alexandra@gmail.com"
+    fill_in 'password', with: "password"
     click_on "Sign In"
-    click_on "Projects"
-    click_on "New Project"
+  end
+
+  scenario 'User can create a project' do
+    click_link("new-pro")
     fill_in 'project[name]', with: "Project1"
     click_on "Create Project"
     expect(page).to have_content("Project1")
@@ -19,52 +21,30 @@ describe 'User can CRUD projects' do
 
 
   scenario 'User can view a show page for a project' do
-    @user = User.create(first_name: "V", last_name: "V", email: "v@v.com", password: "v", password_confirmation: "v")
-    visit '/'
-    click_on "Projects"
-    fill_in 'email', with: "v@v.com"
-    fill_in 'password', with: "v"
-    click_on "Sign In"
-    click_on "Projects"
-    click_on "New Project"
+    click_link("new-pro")
     fill_in 'project[name]', with: "Project2"
     click_on "Create Project"
     expect(page).to have_content("Project2")
-
   end
 
-
   scenario 'User can edit a project' do
-    @user = User.create(first_name: "V", last_name: "V", email: "v@v.com", password: "v", password_confirmation: "v")
-    visit '/'
-    click_on "Projects"
-    fill_in 'email', with: "v@v.com"
-    fill_in 'password', with: "v"
-    click_on "Sign In"
-    click_on "Projects"
-    click_on "New Project"
+    click_link("new-pro")
     fill_in 'project[name]', with: "Project2"
     click_on "Create Project"
     expect(page).to have_content("Project2")
+    click_link("pro-name")
     click_on "Edit"
     fill_in 'project[name]', with: "Project4"
     click_on("Update Project")
     expect(page).to have_content("Project4")
-
   end
 
   scenario 'User can delete a project' do
-    @user = User.create(first_name: "V", last_name: "V", email: "v@v.com", password: "v", password_confirmation: "v")
-    visit '/'
-    click_on "Projects"
-    fill_in 'email', with: "v@v.com"
-    fill_in 'password', with: "v"
-    click_on "Sign In"
-    click_on "Projects"
-    click_on "New Project"
+    click_link("new-pro")
     fill_in 'project[name]', with: "Project2"
     click_on "Create Project"
     expect(page).to have_content("Project2")
+    click_link("pro-name")
     click_on "Delete"
     expect(page).to have_content("Project was successfully deleted.")
   end
