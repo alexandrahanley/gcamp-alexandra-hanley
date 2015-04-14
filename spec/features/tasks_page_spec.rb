ab4825b7f5be2274e1a002ffd2c261ec05171a61
Alexandra Hanley <alexandrahanley@gmail.com>
2015-04-14 15:22:35 -0600

    update tasks CRUD spec

--- spec/features/tasks_page_spec.rb
+++ spec/features/tasks_page_spec.rb
@@ -2,59 +2,42 @@ require 'rails_helper'
 
 describe 'User can CRUD tasks' do
 
-  scenario 'User can create a task' do
-    @user = User.create(first_name: "V", last_name: "V", email: "v@v.com", password: "v", password_confirmation: "v")
-    @project = Project.create(name: "Homework")
-    visit '/'
-    click_on "Signin"
-    fill_in 'email', with: "v@v.com"
-    fill_in 'password', with: "v"
+  before :each do
+    User.create(first_name: "Alexandra", last_name: "Hanley",
+                email: "alexandra@gmail.com", password: "password", admin: true)
+    visit "/signin"
+    fill_in 'email', with: "alexandra@gmail.com"
+    fill_in 'password', with: "password"
     click_on "Sign In"
-    visit '/projects'
-    click_on "Homework"
-    click_on "0 Tasks"
+    click_link("new-pro")
+    fill_in 'project[name]', with: "Project1"
+    click_on "Create Project"
+    expect(page).to have_content("Project1")
+  end
+
+  scenario 'User can create a task' do
     click_on "New Task"
-    fill_in 'task[description]', with: "Proofread"
+    fill_in 'task[description]', with: "My Task"
     click_on "Create Task"
-    expect(page).to have_content("Proofread")
-    expect(page).to have_content("Task was successfully created.")
+    expect(page).to have_content("My Task")
   end
 
 
   scenario 'User can view a show page for a task' do
-    @user = User.create(first_name: "V", last_name: "V", email: "v@v.com", password: "v", password_confirmation: "v")
-    @project = Project.create(name: "Homework")
-    visit '/'
-    click_on "Signin"
-    fill_in 'email', with: "v@v.com"
-    fill_in 'password', with: "v"
-    click_on "Sign In"
-    visit '/projects'
-    click_on "Homework"
-    click_on "0 Tasks"
     click_on "New Task"
-    fill_in 'task[description]', with: "Proofread"
+    fill_in 'task[description]', with: "My Task"
     click_on "Create Task"
-    click_link("task-desc")
-    expect(page).to have_content("Proofread")
-
+    expect(page).to have_content("Task was successfully created.")
+    click_link("My Task")
+    expect(page).to have_content("My Task")
   end
 
 
   scenario 'User can edit a task' do
-    @user = User.create(first_name: "V", last_name: "V", email: "v@v.com", password: "v", password_confirmation: "v")
-    @project = Project.create(name: "Homework")
-    visit '/'
-    click_on "Signin"
-    fill_in 'email', with: "v@v.com"
-    fill_in 'password', with: "v"
-    click_on "Sign In"
-    visit '/projects'
-    click_on "Homework"
-    click_on "0 Tasks"
     click_on "New Task"
-    fill_in 'task[description]', with: "Proofread"
+    fill_in 'task[description]', with: "My Task"
     click_on "Create Task"
+    expect(page).to have_content("Task was successfully created.")
     click_on "Edit"
     fill_in 'task[description]', with: "Proofread-2"
     click_on "Update Task"
@@ -62,19 +45,10 @@ describe 'User can CRUD tasks' do
   end
 
   scenario 'User can delete a task' do
-    @user = User.create(first_name: "V", last_name: "V", email: "v@v.com", password: "v", password_confirmation: "v")
-    @project = Project.create(name: "Homework")
-    visit '/'
-    click_on "Signin"
-    fill_in 'email', with: "v@v.com"
-    fill_in 'password', with: "v"
-    click_on "Sign In"
-    visit '/projects'
-    click_on "Homework"
-    click_on "0 Tasks"
     click_on "New Task"
-    fill_in 'task[description]', with: "Proofread"
+    fill_in 'task[description]', with: "My Task"
     click_on "Create Task"
+    expect(page).to have_content("Task was successfully created.")
     click_link("destroy")
     expect(page).to have_content("Task was successfully destroyed.")
   end
